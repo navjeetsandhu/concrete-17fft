e96a49c3e72860f080b8b2fddfb99765436ab48e
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-08-06 19:26:10 -0400

    changes

--- replaceText.sh
+++ replaceText.sh
@@ -3,3 +3,4 @@
 # Find all files and replace μ with mu
 find "." -type f -exec sed -i '' 's/μ/mu/g' {} \;
 find "." -type f -exec sed -i '' 's/α/alpha/g' {} \;
+find "." -type f -exec sed -i '' 's/Δ/delta/g' {} \;
